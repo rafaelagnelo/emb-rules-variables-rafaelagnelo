e3175d52e6744ef9e997488a9b08fece0db09804
rafaelagnelo <rafaelca13@al.insper.edu.br>
2024-08-15 16:52:09 -0300

    arrumando

--- rules_basic_variable.c
+++ rules_basic_variable.c
@@ -2,20 +2,18 @@
 
 // ..
 
-int a = 0; // var global
-int b = 0; // var global
-
 void foo(int *a) {
     *a = *a + 1; // acessa variavel global
 }
 
-void main(void) {
+int main(void) {
+    int a = 0; // var global
+    int b = 0; // var global
     while (1) {
         foo(&a);
         
         if (a > 5) {
             b = 1;
-            break;
         }
     }
     return 0;
